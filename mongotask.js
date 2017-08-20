ddd27c9f996d7a5d924cfb02a54a54836f3d4917
Leshao Zhang <zhangleshao@gmail.com>
2017-08-20 01:18:54 +0100

    return a function instead of a object

--- mongotask.js
+++ mongotask.js
@@ -9,152 +9,135 @@
  * And also promise is not able to be integrate into libs as it will be 
  * run immediately
  * @author Leshao Zhang, Sisi Guo
- * @version 0.1.1
+ * @version 0.2.1
  * Usage:
- * var db=require('./mongotask');
- * var dbUrl='mongodb://localhost:27017/mydb';
- * db(dbUrl).open(collection);					//return a Task which opens a collection
- * db(dbUrl).find(query).from(collection);      //return a Task which doing a query, no need to open db first or close db in the end
- * db(dbUrl).insert(query).to(collection);      //return a Task which doing a insert, no need to open db first or close db in the end
- * db(dbUrl).distinct(key).from(collection);	//return a Task which doing a distinct, no need to open db first or close db in the end
- * db(dbUrl)....chian(db.close);				//return a Task which close the db.
+ * const dbUrl='mongodb://localhost:27017/mydb';
+ * const db=require('./data.mongoTask')(dbUrl);
+ * db(collection);									//return a Task which opens a collection
+ * db(collection).find(query);     					//return a Task which doing a query, no need to open db first or close db in the end
+ * db(collection).insert(query);   					//return a Task which doing a insert, no need to open db first or close db in the end
+ * db(collection).distinct(key);					//return a Task which doing a distinct, no need to open db first or close db in the end
+ * db(collection)....chain(db.close);				//return a Task which close the db.
  */
 
 'use strict';
-var mongo=require('mongodb').MongoClient;
-var task=require('data.task');
+const mongo=require('mongodb').MongoClient;
+const task=require('data.task');
 
 /**
  * Return the operations to the giving database 
  * @param {String} url 
- * @summary db :: String -> Object
+ * @summary Function :: String -> Object
  */
-var db=(url)=>{
-	var database=null;
+module.exports=(url)=>{
+	let database=null;
 
 	/**
 	 * Return a Task open the giving collection in the database
 	 * @param {String} collection 
-	 * @summary open :: String -> Task
+	 * @summary Function :: String -> Task
 	 */
-	var open=(collection)=>{
-			return new task((reject,result)=>{
-				mongo.connect(url,(err,db)=>{
-					if(err){
-						reject(err);
-					}else{
-						database=db;
-						result(db);
-					}
+	let DB=(collection)=>{
+		let ret={};
+		let col=new task((reject,result)=>{
+			mongo.connect(url,(err,db)=>{
+				if(err){
+					reject(err);
+				}else{
+					database=db;
+					result(db);
+				}
+			})
+		})
+		.map((db)=>{
+			return db.collection(collection);
+		});
+
+		ret.map=col.map;
+		ret.chain=col.chain;
+		/**
+		 * Return a Task close the opened database
+		 * @param {*} res 
+		 * @summary close :: a -> Task(a)
+		 */
+
+		/**
+		 * Return the result giving the query
+		 * @param {Object} query
+		 * @summary find :: Object -> Object -> Object
+		 */
+		ret.find=(query)=>{
+			return col.map((col)=>{
+				return col.find(query); 
+			})
+			.chain((doc)=>{
+				return new task((reject,result)=>{
+					doc.toArray((err,doc)=>{
+						if(err){
+							reject(err);
+						}
+						if(doc){
+							result(doc);
+						}
+					})
 				})
 			})
-			.map((db)=>{
-				return db.collection(collection);
-			});
+			.chain(DB.close)
 		};
 
-	/**
-	 * Return a Task close the opened database
-	 * @param {*} res 
-	 * @summary close :: a -> Task(a)
-	 */
-	var close=(res)=>{
-			return new task((reject,result)=>{
-				if(database){
-					database.close();
-				}
-				result(res);
+		/**
+		 * Return the result giving the key
+		 * @param {String} key 
+		 * @summary distinct :: String -> Object -> Object
+		 */
+		ret.distinct=(key)=>{
+			return col.chain((col)=>{
+				return new task((reject,result)=>{
+					col.distinct(key,(err,doc)=>{
+						if(err){
+							reject(err);
+						}
+						if(doc){
+							result(doc);
+						}
+					})
+				})
 			})
+			.chain(DB.close)
 		};
 
-	/**
-	 * Return the result giving the query
-	 * @param {Object} query
-	 * @summary find :: Object -> Object -> Object
-	 */
-	var find=(query)=>{
-			return {'from':(collection)=>
-						open(collection)
-						.map((col)=>{
-							return col.find(query); 
-						})
-						.chain((doc)=>{
-							return new task((reject,result)=>{
-								doc.toArray((err,doc)=>{
-									if(err){
-										reject(err);
-									}
-									if(doc){
-										result(doc);
-									}
-								})
-							})
-						})
-						.chain(close)
-			}
+		/**
+		 * Return the result of insert a json
+		 * @param {JSON} json 
+		 * @summary insert :: Object -> Object -> Object
+		 */
+		ret.insert=(json)=>{
+			return col.chain((col)=>{
+				return new task((reject,result)=>{
+					col.insert(json,(err,res)=>{
+						if(err){
+							reject(err);
+						}
+						if(res){
+							result(res);
+						}
+					})
+				})
+			})
+			.chain(DB.close)
 		};
 
-	/**
-	 * Return the result giving the key
-	 * @param {String} key 
-	 * @summary distinct :: String -> Object -> Object
-	 */
-	var distinct=(key)=>{
-		return {'from':(collection)=>
-					open(collection)
-					.chain((col)=>{
-						return new task((reject,result)=>{
-							col.distinct(key,(err,doc)=>{
-								if(err){
-									reject(err);
-								}
-								if(doc){
-									result(doc);
-								}
-							})
-						})
-					})
-					.chain(close)
-		}
-	}
-
-	/**
-	 * Return the result of insert a json
-	 * @param {Object} json 
-	 * @summary insert :: Object -> Object -> Object
-	 */
-	var insert=(json)=>{
-		return {"to":(collection)=>
-					open(collection)
-					.chain((col)=>{
-						return new task((reject,result)=>{
-							col.insert(json,(err,res)=>{
-								if(err){
-									reject(err);
-								}
-								if(res){
-									result(res);
-								}
-							})
-						})
-					})
-					.chain(close)
-		}
-	}
-
-	/**
-	 * return the sub functions
-	 */
-	return {
-		"open":open,
-		"find":find,
-		"close":close,
-		"insert":insert,
-		"distinct":distinct
+		return ret;
 	};
-};
 
-/**
- * @module mongotask
- */
-module.exports=db;
+	DB.close=(res)=>{
+		return new task((reject,result)=>{
+			if(database){
+				database.close();
+			}
+			result(res);
+		})
+	};
+
+	return DB;
+};
